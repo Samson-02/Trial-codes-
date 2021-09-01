068b352009091148f1b294f6e7790a2eed738b5c
Samson-02 <88614262+Samson-02@users.noreply.github.com>
2021-09-01 16:41:34 +0530

    ( code with error )

---
+++ Employee details.java
@@ -0,0 +1,35 @@
+public class Main {
+    
+	 class Employee {
+	    String name;
+	    int year;
+	    String address;
+	    
+	    Employee(String name,int year,String address){
+	        this.name = name;
+	        this.year = year;
+	        this.address = address;
+	    }
+	} 
+	      
+    class K{
+         public static void main(String[] args){
+             
+            System.out.println(" EMPLOYEE DETAILS \n");
+            Employee e = new Employee("Robert",1994,"64C-WallsStreet");
+            System.out.print("Name : "+e.name+ "\n");
+            System.out.print("Year of joining : "+e.year+ "\n");
+            System.out.print("Address : "+e.address+ "\n");
+            
+            Employee f = new Employee("Sam",2000,"68D-WallsStreet");
+            System.out.print("Name : "+f.name+ "\n");
+            System.out.print("Year of joining : "+f.year+ "\n");
+            System.out.print("Address : "+f.address+ "\n");
+            
+            Employee g = new Employee("John",1999,"26B-WallsStreet");
+            System.out.print("Name : "+g.name+ "\n");
+            System.out.print("Year of joining : "+g.year+ "\n");
+            System.out.print("Address : "+g.address+ "\n");
+        }
+    }   
+}
